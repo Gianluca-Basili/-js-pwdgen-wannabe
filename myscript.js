dda0e6e1762bf4115dd9e23ed072692a37fd5952
Gianluca-Basili <128229910+Gianluca-Basili@users.noreply.github.com>
2023-05-16 15:22:47 +0200

    add nome cognome e colore

---
+++ myscript.js
@@ -0,0 +1,18 @@
+//inserisci nome
+document.getElementById ("nome");
+let nome= prompt("inserisci nome");
+document.write(nome);
+console.log(nome);
+
+//inserisci cognome
+document.getElementById ("cognome");
+let cognome= prompt("inserisci cognome");
+document.write(cognome);
+console.log(cognome);
+
+//inserisci colore preferito
+document.getElementById ("colore");
+let colore= prompt("inserisci colore prferito");
+document.write(colore);
+console.log(colore);
+
